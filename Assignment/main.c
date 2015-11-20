3befe3c24ba8850db2f165326fdee6e90d59323e
Jose Vives <jota_sk@hotmail.com>
2015-11-20 11:49:01 +0000

    Created struct for a competitor

---
+++ Assignment/main.c
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.c
+ * Author: Jose
+ *
+ * Created on 20 November 2015, 11:31
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "competitor.h"
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
