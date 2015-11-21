6f0b666a92ecfef906d67a3cc886c108b4f0b23f
Jose Vives <jota_sk@hotmail.com>
2015-11-21 19:13:12 +0000

    Removed one header

--- Assignment_2/print.c
+++ Assignment_2/print.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "competitor.h"
+#include "print.h"
 
-void print_competition_table(comp_node_ptr root, char * competition_name, char * competition_date){
+void print_competition_table_function(comp_node_ptr root, char * competition_name, char * competition_date, void (* action)(comp_node_ptr, char*, char*)){
+    action(root, competition_name, competition_date);
+}
+
+void print_competitors_information(comp_node_ptr root, char * competition_name, char * competition_date){
+    
+    printf("Competition: %s. Date: %s", competition_name, competition_date);
+    printf("Competitor Contact Details");
+    
+    void (*action)(comp_node_ptr);
+    
+    print_competition_nodes(root, action);
+    
+}
+
+void print_competition_table_(comp_node_ptr root, char * competition_name, char * competition_date){
     
     printf("Competition: %s. Date: %s", competition_name, competition_date);
     printf("NAME --- Competition Number --- Cucumber --- Carrot --- Runner Bean --- Total Length");
     printf("==========================================================================================");
     
-    print_competition_nodes(root);
+    void(*action)(comp_node_ptr);
+    
+    print_competition_nodes(root, action);
     
 }
 
-void print_competition_nodes(comp_node_ptr node){
+void print_competition_nodes(comp_node_ptr node, void (* action)(comp_node_ptr)){
     
     if(node -> left != NULL){
-        print_competition_nodes(node -> left);
+        print_competition_nodes(node -> left, *action);
     }
+    
+    action(node);
+    
+    if(node -> right != NULL){
+        print_competition_nodes(node -> right, *action);
+    }
+}
+
+void print_node_result(comp_node_ptr node){
     double total = calculateTotalPoints(node);
     int totalFeets = knowFeets(total);
     double totalInches =  knowInches(total);
@@ -34,10 +60,15 @@ void print_competition_nodes(comp_node_ptr node){
             totalFeets,
             totalInches
             );
+}
+
+void print_node_information(comp_node_ptr node){
     
-    if(node -> right != NULL){
-        print_competition_nodes(node -> right);
-    }
+    printf("Competitor Name: ", node -> name);
+    printf("Postal Address: ", node -> addres);
+    printf("Telephone: ", node -> phone_number);
+    printf("");
+    
 }
 
 int knowFeets(double inches){
@@ -53,7 +84,11 @@ double knowInches(double inches){
     
     double inches_result;
     
-    inches_result = inches;
+    int feet;
+    feet = knowFeets(inches);
+    
+    inches_result = (feet * 12) - inches;
     
     return inches_result;
+    
 }
