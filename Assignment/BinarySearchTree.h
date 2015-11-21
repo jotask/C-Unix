0068fdbaf0793b6eda7def9e08d318cf0839398f
Jose Vives <jota_sk@hotmail.com>
2015-11-21 18:50:44 +0000

    Added functions for the BST

---
+++ Assignment/BinarySearchTree.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   BinarySearchTree.h
+ * Author: Jota
+ *
+ * Created on 21 November 2015, 18:39
+ */
+
+#ifndef BINARYSEARCHTREE_H
+#define	BINARYSEARCHTREE_H
+
+comp_node_ptr read_competitor_node(int id, FILE * competitor_filename);
+void add_competitor(comp_node_ptr current, comp_node_ptr new);
+double calculateTotalPoints(comp_node_ptr node);
+double convertToInches(int feets, double inches);
+
+#endif	/* BINARYSEARCHTREE_H */
+
