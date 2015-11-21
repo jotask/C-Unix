1d5248ef16b3917a5faaa626051721c1dfc6603f
Jose Vives <jota_sk@hotmail.com>
2015-11-21 18:49:50 +0000

    Added print header

---
+++ Assignment/print.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   print.h
+ * Author: Jota
+ *
+ * Created on 21 November 2015, 18:39
+ */
+
+#ifndef PRINT_H
+#define	PRINT_H
+
+void print_competition_nodes(comp_node_ptr node);
+int knowFeets(double inches);
+double knowInches(double inches);
+
+#endif	/* PRINT_H */
+
