98c80ab84f54582f61708af0d5f05126600391f6
Jose Vives <jota_sk@hotmail.com>
2015-11-22 17:08:24 +0000

    Deleted the math header and change a function name

--- Assignment/BinarySearchTree.c
+++ Assignment/BinarySearchTree.c
@@ -3,6 +3,21 @@
 #include "competitor.h"
 #include "BinarySearchTree.h"
 
+/* 
+ * 
+ * Contains all the functions for build the tree for a competition
+ * 
+ * File:   BinarySearchTree.c
+ * Author: Jose Vives
+ *
+ * Created on 19 November 2015, 16:42
+ */
+
+/*
+ * Create tree reading the file, and creating his node from the information
+ * provided by the file. Return the root node from the tree, for keep his
+ * reference.
+ */
 comp_node_ptr addCompetitorTable(char * competitor_table_filename, char * competition_name, char * competition_date){
     
     int id;
@@ -15,25 +30,24 @@ comp_node_ptr addCompetitorTable(char * competitor_table_filename, char * compet
     
     competitor_table_file = fopen(competitor_table_filename, "r");
     
-    if(root == NULL){
+    if(competitor_table_file == NULL){
         
         printf("Can't open %s\n",competitor_table_filename);
         return NULL;
         
-    } else {
-        
-        fscanf(competitor_table_file, "%s", competition_name);
-        fscanf(competitor_table_file, "%s", competition_date);
-        
-        root = read_competitor_node(id, competitor_table_file);
-        
-        do{
-            id++;
-            tmp_node = read_competitor_node(id, competitor_table_file);
-            add_competitor(root, tmp_node);
-        }while(tmp_node != NULL);
-        
     }
+        
+    fscanf(competitor_table_file, "%s", competition_name);
+    fscanf(competitor_table_file, "%s", competition_date);
+
+    root = read_competitor_node(id, competitor_table_file);
+
+    do{
+        id++;
+        tmp_node = read_competitor_node(id, competitor_table_file);
+        add_competitor(root, tmp_node);
+    }while(tmp_node != NULL);
+        
     fclose(competitor_table_file);
     return root;
 }
@@ -46,9 +60,7 @@ comp_node_ptr read_competitor_node(int id, FILE * competitor_filename){
     
     tmp_node = calloc(1, sizeof(tmp_node));
     
-    scan_status = fscanf(competitor_filename, "%s",
-            tmp_node -> name
-            );
+    scan_status = fscanf(competitor_filename, "%s", tmp_node -> name );
     
     if(scan_status == EOF){
         printf("Problem reading file. \n");
@@ -70,16 +82,27 @@ comp_node_ptr read_competitor_node(int id, FILE * competitor_filename){
     
 }
 
+/*
+ *  Function for add a competitor previously created to the tree. This function
+ * decide to which direction needs to be this new node on the tree, for placed
+ * it to his correct place
+ */
 void add_competitor(comp_node_ptr current, comp_node_ptr new){
     
+    /* Error check, if the the node we want add is null, we can add this to
+     * the tree, so just skip return null */
     if(new == NULL){
         return;
     }
+    /* Calculate the total length for the current node */
+    double lengthCurrent = calculateTotalLength(current);
     
-    double pointsCurrent = calculateTotalPoints(current);
-    double pointsNew = calculateTotalPoints(new);
+    /* Calculate the total length for the new node we want to add */
+    double lengthNew = calculateTotalLength(new);
     
-    if(pointsCurrent <= pointsNew){
+    /* Check if the new node total length is greater or equal to the 
+     * current node for decide which direction we need go deeper in the tree */
+    if(lengthCurrent <= lengthNew){
         /* we need to add new node on right subtree as
            it has less points than current node */
         if (current -> right == NULL) {
@@ -105,10 +128,13 @@ void add_competitor(comp_node_ptr current, comp_node_ptr new){
         }
     }
     
-    
 }
 
-double calculateTotalPoints(comp_node_ptr node){
+/*
+ * Calculate the total length from one node summing all the length of
+ * all the fruits his have
+ */
+double calculateTotalLength(comp_node_ptr node){
     
     double points;
     points = 0;
@@ -121,6 +147,9 @@ double calculateTotalPoints(comp_node_ptr node){
     
 }
 
+/*
+ * Convert feet and inches, to the sum of those values in inches
+ */
 double convertToInches(int feets, double inches){
     
     double totalInches;
